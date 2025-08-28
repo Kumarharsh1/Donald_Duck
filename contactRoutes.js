80a658c449f2f436b75e098490c04a5164bb7020
KUMAR HARSH <93419195+Kumarharsh1@users.noreply.github.com>
2025-08-28 16:54:30 +0530

    contactRoutes.js

--- contactRoutes.js
+++ contactRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Contact = require('../models/Contact');
+const Contact = require('./Contact');  // CHANGED FROM '../models/Contact'
 
 // POST new contact message
 router.post('/', async (req, res) => {
